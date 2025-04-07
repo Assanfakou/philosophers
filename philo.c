634e6f2dfae55d372259641029fe860c5244e9cc
Hassan Fakou <hfakou@e2r6p12.1337.ma>
2025-04-07 20:13:47 +0100

    taking the handler

--- philo.c
+++ philo.c
@@ -1,62 +1,89 @@
 #include "philo.h"
 
-// void *action(void *i)
-// {
-//     if (i == 1)
-//         printf("philo eat 1\n");
-//     else if (i == 2)
-//         printf("philo eat 2\n");
-//     else if (i == 3)
-//         printf("philo eat 3\n");
-//     else if (i == 4)
-//         printf("philo eat 4\n");   
-//     sleep(2);
-//     printf("philo think\n");
-// };
+void pars_data(t_philo_info *data, char **av, int size)
+{
+    int i = 0;
 
-void pars_data(t_data *data, char **av)
-{
-    data->philo_number = atoi(av[1]);
-    data->time_t_die = atoi(av[2]);
-    data->time_t_eat = atoi(av[3]);
-    data->time_t_sleep = atoi(av[4]);
-    data->count_philo_eat = atoi(av[5]);
+    while (i < size)
+    {
+        data[i].data.philo_number = atoi(av[1]);
+        data[i].data.time_t_die = atoi(av[2]) * 1000;
+        data[i].data.time_t_eat = atoi(av[3]) * 1000;
+        data[i].data.time_t_sleep = atoi(av[4]) * 1000;
+        data[i].data.count_philo_eat = atoi(av[5]);    
+        i++;
+    }
 }
-void *func()
+void handle_state_philo(t_philo_info *info)
 {
-    printf("hello from threads\n");
-    sleep(1); 
+    if (info->state == 0)
+    {
+        printf("%ld %d is eating\n",info->data.time_t_eat, info->id);
+        // usleep(info->data.time_t_eat);
+        info->state = 1;
+        // printf("%d, %ld, %ld, %ld, %ld\n", info->data.philo_number, info->data.time_t_die, info->data.time_t_eat, info->data.time_t_sleep, info->data.count_philo_eat);
+
+    }
+}
+void *func(void *args)
+{
+    int i = gettid();
+    t_philo_info *arg = (t_philo_info *) args; 
+    handle_state_philo(arg);
+    // printf("treaeds id :[%d]\n", i);
+    // if (arg->id == 0)
+    //     printf("thread 0\n");
+    // else if (arg->id == 1)
+    //     printf("thread 1\n");
+    // else if (arg->id == 2)
+    //     printf("thread 2\n");
+    // else if (arg->id == 3)
+    //     printf("tread 3\n");
+    // printf("hello from threads\n");
+    pthread_exit(NULL);
 }
 
 int main(int ac, char **av)
 {
     int i = 0;  
-    
-    t_data data;
     t_philo thds;
-
+    int num_philo = atoi(av[1]);
+    
     if (ac != 6)
         return (1);
-    pars_data(&data, av);
-    thds.mutexes = malloc(sizeof(pthread_mutex_t) * data.philo_number);
-    while (i < data.philo_number)
+    t_philo_info info[num_philo];
+    pars_data(info, av, num_philo);
+    // memset(info, '0', info->data.philo_number);
+    int i_info = 0;
+    while (i_info < info->data.philo_number)
+    {
+        info[i_info].id = i_info;
+        info[i_info].state = 0;
+        info[i_info].left_fork = thds.mutexes;
+        info[i_info].time_t_eatt = 1000;
+        i_info++;
+    }
+    
+    thds.mutexes = malloc(sizeof(pthread_mutex_t) * info->data.philo_number);
+    while (i < info->data.philo_number)
     {
         if (pthread_mutex_init(&thds.mutexes[i], NULL) != 0)
             return (2);
         i++;
     }
-    thds.th = malloc(sizeof(pthread_t) * data.philo_number);
+    thds.th = malloc(sizeof(pthread_t) * info->data.philo_number);
     i = 0;
-    while (i < data.philo_number)
+    while (i < info->data.philo_number)
     {
-        if (pthread_create(&thds.th[i], NULL, &func, NULL) != 0)
+        if (pthread_create(&thds.th[i], NULL, &func, &info[i]) != 0)
             return (3);
+
         if (pthread_join(thds.th[i], NULL) != 0)
             return (4);
-        i++;
+            i++;
     }
     free(thds.mutexes);
-    // printf("%d, %d, %d, %d, %d", data.philo_number, data.time_t_die, data.time_t_eat, data.time_t_sleep, data.count_philo_eat);
+    printf("%d, %ld, %ld, %ld, %ld", info->data.philo_number, info->data.time_t_die, info->data.time_t_eat, info->data.time_t_sleep, info->data.count_philo_eat);
 
     // pthread_create(&t1, NULL, &action, i + 1);
     // pthread_join(t1, NULL);
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: hfakou <hfakou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 10:35:13 by hfakou            #+#    #+#             */
-/*   Updated: 2025/04/07 12:54:54 by hfakou           ###   ########.fr       */
+/*   Updated: 2025/04/07 19:49:57 by hfakou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,23 @@
 typedef struct s_data
 {
     int philo_number;
-    unsigned int time_t_die;
-    unsigned int time_t_eat;
-    unsigned int time_t_sleep;
-    unsigned int count_philo_eat;
+    unsigned long int time_t_die;
+    unsigned long int time_t_eat;
+    unsigned long int time_t_sleep;
+    unsigned long int count_philo_eat;
 }   t_data;
 typedef struct s_philo
 {
     pthread_mutex_t *mutexes;
     pthread_t *th;
 } t_philo;
+
+typedef struct s_philo_info
+{
+    int id;
+    int state;
+    unsigned long int time_t_eatt;
+    pthread_mutex_t *right_fork;
+    pthread_mutex_t *left_fork;
+    t_data data;
+}  t_philo_info;
